64070fc585dc7f13092166db6f56759575c1d942
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-02-08 21:09:26 +0200

    made task1

---
+++ homework_07/js/task1.js
@@ -0,0 +1,27 @@
+let n = +prompt("Enter a natural number between 0 and 20");
+
+if (n == 0 || n > 20) {
+    console.error("Incorrect!");
+} else {
+    console.log(makePiramid(n));
+}
+
+function makePiramid(a) {
+    let message = "",
+        s1 = "[~]",
+        s2 = "   ",
+        s3 = "\n",
+        spases = a - 1;
+        
+    for (let i=1; i<=a; i++) {
+        for (let j=0; j<spases; j++) {
+          message += s2;
+        }
+        for (let z=0; z<2*i-1; z++) {
+          message += s1;
+        }
+        message += s3;
+        spases--;
+    }
+    return message;
+}
