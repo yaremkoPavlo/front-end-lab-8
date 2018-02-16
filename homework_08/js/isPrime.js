5ef07f68782713603746c3b8e98d606b7ca5a885
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-02-16 14:14:48 +0200

    update isPrime function

--- homework_08/js/isPrime.js
+++ homework_08/js/isPrime.js
@@ -1,11 +1,11 @@
 function isPrime(n) {
-    if (n == 2 || n == 3) {
+    if (n === 2 || n === 3) {
         return true;
-    } else if (n%2 == 0 || n%3 == 0) {
+    } else if (n%2 === 0 || n%3 === 0) {
         return false;
     } else {
         for (let i = 1; 6*i-1 <= n; i++) {
-            if (i*6-1 == n || i*6+1 == n) {
+            if (i*6-1 === n || i*6+1 === n) {
                 return true;
             }
         }
