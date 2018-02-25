57b5c9d1a6389458c6275b5dc5d9a38585b9552f
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-02-25 21:39:23 +0200

    add empty folder property

--- homework_11/app.js
+++ homework_11/app.js
@@ -4,15 +4,24 @@ var rootNode = document.getElementById("root");
 function renderFolder (folder) {
 	let ul = document.createElement("ul"),
 			li = document.createElement("li"),
+			liEmp = document.createElement("li"),
 			text = document.createTextNode(folder["title"]),
 			empty = document.createTextNode("Folder is empty");
 	li.appendChild(text);
 	ul.appendChild(li);
+
+	if (folder["children"] === null || folder["children"] === false) {
+		liEmp.appendChild(empty);
+		li.appendChild(liEmp);
+		ul.appendChild(li);
+	}
+
 	if (folder["children"]) {
 		for (let i = 0; i < folder["children"].length; i++) {
 			li.appendChild(renderFolder(folder["children"][i]))
 		}
 	}
+
 	if (folder["folder"]) {
 		li.className = "folder";
 	} else {
