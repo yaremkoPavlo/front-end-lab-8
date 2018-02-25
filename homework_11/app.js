d690ac6505e8af2f1a222cbfa64d7faa28c7cd69
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-02-25 20:49:51 +0200

    render tree of folders

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,5 +1,24 @@
 var rootNode = document.getElementById("root");
 
 // Your code goes here
+function renderFolder (folder) {
+	let ul = document.createElement("ul"),
+			li = document.createElement("li"),
+			text = document.createTextNode(folder["title"]),
+			empty = document.createTextNode("Folder is empty");
+	li.appendChild(text);
+	ul.appendChild(li);
+	if (folder["children"]) {
+		for (let i = 0; i < folder["children"].length; i++) {
+			li.appendChild(renderFolder(folder["children"][i]))
+		}
+	}
+	if (folder["folder"] === false) {
+		ul.className("nofolder");
+	}
+	return ul;
+}
 
-rootNode.appendChild(/* Append your TreeView node*/);
+
+
+rootNode.appendChild(renderFolder(structure[0]));
